bb53296b7354ebe4f09852c7ae1ad9842d4c1c88
Elysium <48912327+ElysiumRL@users.noreply.github.com>
2022-09-30 22:02:52 +0200

    Initial Commit

---
+++ Source/ProceduralGeneration/Public/ProceduralGenerationRules.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h" 
+
+
+#include "UObject/NoExportTypes.h"
+#include "ProceduralGenerationRules.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGenerationType : public FTableRowBase
+{
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	FString type;
+};
+
+UCLASS()
+class PROCEDURALGENERATION_API UProceduralGenerationRules : public UObject
+{
+	GENERATED_BODY()
+private:
+
+protected:
+
+public:
+
+
+
+};
