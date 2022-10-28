c69818e98c485240049407c1e2009c3560da0535
Elysium <48912327+ElysiumRL@users.noreply.github.com>
2022-10-28 17:09:35 +0200

    Moved Enhanced Box,& dynamic wall improvement

---
+++ Plugins/ProceduralGenerationTool/Source/ProceduralGenerationTool/Private/EnhancedBox.cpp
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnhancedBox.h"
+#include <Kismet/KismetMathLibrary.h>
+
+UEnhancedBox::UEnhancedBox(const FVector& origin, const FVector& extent, float _rotation, const FIntVector& _relativeLocation)
+{
+	subdivisions = TArray<UEnhancedBox>();
+	box = FBox::BuildAABB(origin, extent);
+	this->origin = origin;
+	this->extent = extent;
+	relativeLocation = _relativeLocation;
+	rotation = _rotation;
+	GenerateVertices();
+}
+
+UEnhancedBox::UEnhancedBox(const FVector& origin, const FVector& extent, float _rotation, const FIntVector& _relativeLocation, const UEnhancedBox& centralBox)
+{
+	subdivisions = TArray<UEnhancedBox>();
+	box = FBox::BuildAABB(origin, extent);
+	relativeLocation = _relativeLocation;
+	this->origin = origin;
+	this->extent = extent;
+	rotation = _rotation;
+	GenerateVertices(centralBox);
+}
+
+void UEnhancedBox::GenerateVertices(const UEnhancedBox& _centralBox)
+{
+	TArray<FVector> allPoints = TArray<FVector>();
+	allPoints.Add(box.Min);										// 1
+	allPoints.Add(FVector(box.Max.X, box.Min.Y, box.Min.Z));	// 2
+	allPoints.Add(FVector(box.Max.X, box.Max.Y, box.Min.Z));	// 3
+	allPoints.Add(FVector(box.Min.X, box.Max.Y, box.Min.Z));	// 4
+	allPoints.Add(FVector(box.Max.X, box.Min.Y, box.Max.Z));	// 5
+	allPoints.Add(FVector(box.Min.X, box.Min.Y, box.Max.Z));	// 6
+	allPoints.Add(FVector(box.Min.X, box.Max.Y, box.Max.Z));	// 7
+	allPoints.Add(box.Max);										// 8
+
+	for (int i = 0; i < allPoints.Num(); i++)
+	{
+		allPoints[i] = RotateBox(_centralBox.box.GetCenter(), allPoints[i], rotation * UKismetMathLibrary::GetPI() / 180.f);
+	}
+
+	vertices = allPoints;
+}
+
+void UEnhancedBox::GenerateVertices()
+{
+	TArray<FVector> allPoints = TArray<FVector>();
+	allPoints.Add(box.Min);										// 1
+	allPoints.Add(FVector(box.Max.X, box.Min.Y, box.Min.Z));	// 2
+	allPoints.Add(FVector(box.Max.X, box.Max.Y, box.Min.Z));	// 3
+	allPoints.Add(FVector(box.Min.X, box.Max.Y, box.Min.Z));	// 4
+	allPoints.Add(FVector(box.Max.X, box.Min.Y, box.Max.Z));	// 5
+	allPoints.Add(FVector(box.Min.X, box.Min.Y, box.Max.Z));	// 6
+	allPoints.Add(FVector(box.Min.X, box.Max.Y, box.Max.Z));	// 7
+	allPoints.Add(box.Max);										// 8
+
+	for (int i = 0; i < allPoints.Num(); i++)
+	{
+		allPoints[i] = RotateBox(box.GetCenter(), allPoints[i], rotation * UKismetMathLibrary::GetPI() / 180.f);
+	}
+
+	vertices = allPoints;
+
+}
+
+void UEnhancedBox::DrawBox(IToolsContextRenderAPI* RenderAPI, const FColor& _color /*= FColor::Red*/, float thickness /*= 2.f*/)
+{
+	// Vertices must be precisely at in the format of GetAllBoxVertices() in order to work
+	DrawLine(RenderAPI, vertices[5], vertices[4], _color, thickness);
+	DrawLine(RenderAPI, vertices[6], vertices[7], _color, thickness);
+	DrawLine(RenderAPI, vertices[0], vertices[1], _color, thickness);
+	DrawLine(RenderAPI, vertices[3], vertices[2], _color, thickness);
+	DrawLine(RenderAPI, vertices[0], vertices[5], _color, thickness);
+	DrawLine(RenderAPI, vertices[1], vertices[4], _color, thickness);
+	DrawLine(RenderAPI, vertices[3], vertices[6], _color, thickness);
+	DrawLine(RenderAPI, vertices[2], vertices[7], _color, thickness);
+	DrawLine(RenderAPI, vertices[4], vertices[7], _color, thickness);
+	DrawLine(RenderAPI, vertices[1], vertices[2], _color, thickness);
+	DrawLine(RenderAPI, vertices[5], vertices[6], _color, thickness);
+	DrawLine(RenderAPI, vertices[0], vertices[3], _color, thickness);
+
+}
+
+void UEnhancedBox::DrawLine(IToolsContextRenderAPI* RenderAPI, const FVector& start, const FVector& end, const FColor& _color /*= FColor::Red*/, float thickness /*= 2.f*/)
+{
+	//From the InteractiveTool demo
+	auto PDI = RenderAPI->GetPrimitiveDrawInterface();
+	// draw a thin line that shows through objects
+	PDI->DrawLine(start, end, _color, SDPG_Foreground, thickness, 0.0f, true);
+	// draw a thicker line that is depth-tested
+	PDI->DrawLine(start, end, _color, SDPG_World, thickness, 0.0f, true);
+
+}
+
+FVector UEnhancedBox::RotateBox(const FVector& boxOrigin, FVector fromLocation, float angle)
+{
+	float sinAngle = sin(angle);
+	float cosAngle = cos(angle);
+
+	fromLocation.X -= boxOrigin.X;
+	fromLocation.Y -= boxOrigin.Y;
+
+	float xnew = fromLocation.X * cosAngle - fromLocation.Y * sinAngle;
+	float ynew = fromLocation.X * sinAngle + fromLocation.Y * cosAngle;
+
+	fromLocation.X = xnew + boxOrigin.X;
+	fromLocation.Y = ynew + boxOrigin.Y;
+
+	return FVector(fromLocation.X, fromLocation.Y, fromLocation.Z);
+
+}
+
---
+++ Plugins/ProceduralGenerationTool/Source/ProceduralGenerationTool/Public/EnhancedBox.h
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include <ToolContextInterfaces.h>
+
+
+/**
+ * 
+ */
+
+class PROCEDURALGENERATIONTOOL_API UEnhancedBox
+{
+
+public:
+
+		UEnhancedBox() = default;
+
+		UEnhancedBox(const FVector& origin, const FVector& extent, float _rotation = 0.0f, const FIntVector& _relativeLocation = FIntVector::ZeroValue);
+
+		UEnhancedBox(const FVector& origin, const FVector& extent, float _rotation, const FIntVector& _relativeLocation, const UEnhancedBox& centralBox);
+
+		FBox box;
+
+		TArray<FVector> vertices;
+
+		FVector origin;
+
+		FVector extent;
+
+		FIntVector relativeLocation;
+
+		float rotation;
+
+		TArray<UEnhancedBox> subdivisions;
+
+		FColor color;
+
+		//Generates all the vertices of the Box
+		void GenerateVertices(const UEnhancedBox& _centralBox);
+
+		void GenerateVertices();
+
+		void DrawBox(IToolsContextRenderAPI* RenderAPI, const FColor& _color = FColor::Red, float thickness = 2.f);
+
+		void DrawLine(IToolsContextRenderAPI* RenderAPI, const FVector& start, const FVector& end, const FColor& _color = FColor::Red, float thickness = 2.f);
+
+		FVector RotateBox(const FVector& boxOrigin, FVector fromLocation, float angle);
+
+		FORCEINLINE float Area2D() { return extent.X * extent.Y; }
+
+		FORCEINLINE float Area3D() { return extent.X * extent.Y * extent.Z; }
+
+		FORCEINLINE float Perimeter2D() { return 2 * extent.X + 2 * extent.Y; }
+
+		FORCEINLINE float Width() { return extent.X; }
+
+		FORCEINLINE float Height() { return extent.Y; }
+
+		FORCEINLINE float WidthRatio() { return Height() == 0.0f ? 0.0f : Width() / Height(); }
+
+		FORCEINLINE float HeightRatio() { return Width() == 0.0f ? 0.0f : Height() / Width(); }
+
+		FORCEINLINE FVector TopLeft() { return box.Min; }
+
+		FORCEINLINE FVector TopRight() { return FVector(box.Min.X, box.Max.Y, box.Min.Z); }
+
+		FORCEINLINE FString ToString() { return FString::Printf(TEXT("Origin : %s - Extent : %s"), *origin.ToString(), *extent.ToString()); }
+};
