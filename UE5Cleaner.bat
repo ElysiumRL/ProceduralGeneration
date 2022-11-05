39a83cfb77f572ebb06d5ce5a2f1668094bef55f
Elysium <48912327+ElysiumRL@users.noreply.github.com>
2022-11-05 15:51:54 +0100

    Update UE5Cleaner.bat

--- UE5Cleaner.bat
+++ UE5Cleaner.bat
@@ -13,6 +13,7 @@ rmdir /q/s Intermediate
 rmdir /q/s Saved
 
 set path=""
+
 @echo on
 for /F "delims=" %%I in ('dir /s /b *.uproject') do (path="%%I")
 start /b /d "C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\DotNET\UnrealBuildTool\" UnrealBuildTool.exe -projectfiles -project=%path% -game -rocket -progress
