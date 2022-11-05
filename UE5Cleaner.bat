841b28448d24ab402548f146388d6a1e17154e39
Elysium <48912327+ElysiumRL@users.noreply.github.com>
2022-11-05 15:32:13 +0100

    Create UE5Cleaner.bat
    Le cleaner delete tous les fichiers temp (+ saved) et régen automatiquement les fichiers d'unreal
    
    Par contre faut que le moteur soit à son emplacement par défaut (C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\DotNET\UnrealBuildTool\)

---
+++ UE5Cleaner.bat
@@ -0,0 +1,18 @@
+@echo off
+DEL /q/s *.sln
+DEL /q/s .vs
+DEL /q/s Binaries
+DEL /q/s DerivedDataCache
+DEL /q/s Intermediate
+DEL /q/s Saved
+
+rmdir /q/s .vs
+rmdir /q/s Binaries
+rmdir /q/s DerivedDataCache
+rmdir /q/s Intermediate
+rmdir /q/s Saved
+
+set path=""
+@echo on
+for /F "delims=" %%I in ('dir /s /b *.uproject') do (path="%%I")
+start /b /d "C:\Program Files\Epic Games\UE_5.0\Engine\Binaries\DotNET\UnrealBuildTool\" UnrealBuildTool.exe -projectfiles -project=%path% -game -rocket -progress
